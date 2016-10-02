d0848efd8c3087055da145e9a83af541f6b1cfe2
hash-m <javed.shah@rutgers.edu>
2016-10-02 19:12:34 -0400

    BorderPane basics

--- SongLibrary/src/songLibrary/SongLib.java
+++ SongLibrary/src/songLibrary/SongLib.java
@@ -2,18 +2,21 @@ package songLibrary;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.fxml.*;
 
 
 public class SongLib extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getClassLoader().getResource("MainUI.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Shwasty Song Collection");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
