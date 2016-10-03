4cf2560d5d0b45f1d1aba2c2f3d2586f708830f9
hash-m <javed.shah@rutgers.edu>
2016-10-02 23:13:11 -0400

    ListView in view controller

--- SongLibrary/src/songLibrary/SongLibController.java
+++ SongLibrary/src/songLibrary/SongLibController.java
@@ -2,12 +2,26 @@ package songLibrary;
 
 import java.util.Collections;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class SongLibController {
 	
-	private ObservableList<Song> playList;
+	@FXML
+	ListView<String> lv;
 	
+	private ObservableList<Song> playList;
+	private ObservableList<String> playListStrings;
+	
+	public void start(Stage mainStage){
+		playListStrings = FXCollections.observableArrayList("elephants");
+
+		lv.setItems(playListStrings);
+		lv.getSelectionModel().select(0);
+	}
 	public void addSong(String name,String artist, String album, String year){
 		
 		Song song = new Song(name, artist, album, year);
@@ -18,7 +32,5 @@ public class SongLibController {
 		else{
 			//Collections.sort(playList.name);
 		}
-		
 	}
-	
 }
