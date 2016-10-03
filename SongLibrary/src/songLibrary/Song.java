c461aa463e1fd5f825f066b3ddceb84aeba17d25
cacheOverEverything <ishan.pal@rutgers.edu>
2016-10-02 21:55:42 -0400

    added setters and getters working on add method

--- SongLibrary/src/songLibrary/Song.java
+++ SongLibrary/src/songLibrary/Song.java
@@ -1,5 +1,50 @@
 package songLibrary;
 
 public class Song {
-
+	
+	private String name;
+	private String artist;
+	private String album;
+	private String year;
+	
+	public Song(String name, String artist, String album, String year){
+		
+		this.name=name;
+		this.artist=artist;
+		this.album=album;
+		this.year=year;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getArtist(){
+		return artist;
+	}
+	
+	public String getAlbum(){
+		return album;
+	}
+	
+	public String getYear(){
+		return year;
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	public void setArtist(String artist){
+		this.artist=artist;
+	}
+	
+	public void setAlbum(String album){
+		this.album=album;
+	}
+	
+	public void setYear(String year){
+		this.year=year;
+	}
+	
 }
--- SongLibrary/src/songLibrary/SongLibController.java
+++ SongLibrary/src/songLibrary/SongLibController.java
@@ -1,5 +1,28 @@
 package songLibrary;
 
+import java.util.Collections;
+
+import javafx.collections.ObservableList;
+
 public class SongLibController {
+	
+	private ObservableList<Song> playList;
+	
+	public void addSong(String name,String artist, String album, String year){
+		
+		Song song = new Song(name, artist, album, year);
+		
+		if(playList.isEmpty()){
+			playList.add(song);
+		}
+		else{
+			//Collections.sort(playList.name);
+		}
+		
+		
+		
+	}
+	
+	
 
 }
