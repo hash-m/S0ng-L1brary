7e0d58ae88821b03697c12ea63c16aa5199eb1ed
hash-m <javed.shah@rutgers.edu>
2016-10-02 20:27:20 -0400

    Start class reformat

--- SongLibrary/src/songLibrary/SongLib.java
+++ SongLibrary/src/songLibrary/SongLib.java
@@ -12,9 +12,10 @@ public class SongLib extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getClassLoader().getResource("MainUI.fxml"));
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/MainUI.fxml"));
+			BorderPane root = (BorderPane)loader.load();
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Shwasty Song Collection");
 			primaryStage.show();
@@ -27,5 +28,3 @@ public class SongLib extends Application {
 		launch(args);
 	}
 }
-
-/* Test 222*/
